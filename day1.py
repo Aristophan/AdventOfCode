f95dffc7598cb0ff8391f74e2cca84f2a1d7ba00
Antoine Burgaud <antoine.burgaud.35@gmail.com>
2023-12-01 17:34:27 +0200

    ex1 part1 and 2 solved

--- day1.py
+++ day1.py
@@ -1,1 +1,18 @@
+numbers = list(map(str,range(10)))
+data = [l.strip() for l in open('input.txt')]
+#part1
+X = [[int(char) for char in l if char in numbers]for l in data]
+print(sum([10*l[0]+l[-1] for l in X]))
 
+#part2
+sol2 = 0
+for ligne in data:
+    digits = []
+    for i,c in enumerate(ligne):
+        if c.isdigit():
+            digits.append(c)
+        for num,d in enumerate(["one","two","three","four","five","six","seven","eight","nine"]):
+            if ligne[i:].startswith(d):
+                digits.append(str(num+1))
+    sol2+=int(digits[0]+digits[-1])
+print(sol2)

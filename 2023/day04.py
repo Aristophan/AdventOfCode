6a5a69a65eeacfc57e047d761029b09a333d616a
Antoine Burgaud <antoine.burgaud.35@gmail.com>
2023-12-04 07:59:08 +0200

    cleaning

--- 2023/day04.py
+++ 2023/day04.py
@@ -2,22 +2,19 @@ import utils as u
 import numpy as np
 
 data = open('inputs/4.txt').read().strip().replace('  ',' 0')
-
 N = len(data.split('\n'))
 
 sol1 = 0
-sol2 = np.ones(N)
-sol1_1 = 0
-
+sol2 = np.ones(N,dtype=int)
 for i,line in enumerate(data.split('\n')):
     ticket=0
     left,right = line.split(':')[1].strip().split('|')
 
-    winning = set([int(char.strip()) for char in left.strip().split(' ')])
-    play = set([int(char.strip()) for char in right.strip().split(' ')])
+    winning = set([int(char) for char in left.strip().split(' ')])
+    play = set([int(char) for char in right.strip().split(' ')])
 
     ticket = len(winning.intersection(play))
-    prize = 2**(ticket-1) if ticket>0 else 0
+    prize = int(2**(ticket-1)) #int(0.5)=0
 
     sol1+=prize
 
@@ -26,5 +23,5 @@ for i,line in enumerate(data.split('\n')):
     elif i<N:
         sol2[i+1:] += sol2[i]
 
-print(f'sol1 : {int(sol1)}')
-print(f'sol2 : {int(sum(sol2))}')
+print(f'sol1 : {sol1}')
+print(f'sol2 : {sum(sol2)}')
